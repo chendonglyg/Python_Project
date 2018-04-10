0cf91cbc22b14a53a38f86c9eff450a47a411685
chendonglyg <13305138800@189.cn>
2018-04-10 16:06:50 +0800

    PushToMacTest

---
+++ Project_Learn/Helloworld.py
@@ -0,0 +1,37 @@
+import pip
+print(100)
+print(100+200)
+
+
+"""
+
+name=input()
+print('''您\"\'录入
+...的名字是：''',name)
+
+"""
+
+print(3>1)
+
+print('''line1
+line2
+line3''')
+
+age=39
+
+if age>=30:
+    print("成人")
+else:
+    print("孩纸")
+
+classmates=["豆豆","yangyang","xiongxiong","qianqian"]
+
+print(len(classmates))
+
+class Student(object):
+   pass
+
+fromstud=Student()
+
+print(fromstud)
+
